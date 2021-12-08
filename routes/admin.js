5fed71c02b627100d1066cfc85ed72e94d094c0f
rashida-mahroof <rashidamahroofp@gmail.com>
2021-12-08 17:38:28 +0530

    adminpage setup

---
+++ routes/admin.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  let products = [
+    {
+      name : "IPHONE 11 PRO",
+      category : "MObile",
+      description : "lorem ipsem sit amet dolor it.",
+      image : "https://static.toiimg.com/thumb/resizemode-4,msid-71395006,imgsize-500,width-800/71395006.jpg"
+    },
+    {
+      name : "OPPO F11",
+      category : "MObile",
+      description : "lorem ipsem sit amet dolor it.",
+      image : "https://img.tatacliq.com/images/i6/1348Wx2000H/MP000000004463836_1348Wx2000H_20200805234048.jpeg"
+    },
+    {
+      name : " SAMSUNG S20",
+      category : "MObile",
+      description : "lorem ipsem sit amet dolor it.",
+      image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHFjGQl7NRWDEBQbXdWzxNOk172p0tm3HlzQ&usqp=CAU"
+    },
+    {
+      name : "REDMI NOTE 10",
+      category : "MObile",
+      description : "lorem ipsem sit amet dolor it.",
+      image : "https://static.digit.in/default/56df7a071797e0355cc2f6f95093fefd89c43c94.jpeg?tr="
+    }
+  ]
+
+  res.render('admin/view-products',{admin:true,products});
+});
+router.get('/add-product',function(req,res){
+  res.render('admin/add-product')
+})
+router.post('/add-product',(req,res)=>{
+  console.log(req.body)
+  console.log(req.files.Image)
+})
+module.exports = router;
